dd55b1718f8c6a30ccf4596904b47c8a0cbc311e
kozyrek <kozyrevaia@bk.ru>
2022-04-11 16:20:06 +0500

    исправляет замечания

--- js/photo.js
+++ js/photo.js
@@ -2,12 +2,12 @@ const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const fieldAvatarUpload = document.querySelector('#avatar');
 const avatarPreview = document.querySelector('.ad-form-header__preview img');
-const fieldFotoUpload = document.querySelector('#images');
-const fotoPreviewContainer = document.querySelector('.ad-form__photo');
+const fieldPhotoUpload = document.querySelector('#images');
+const photoPreviewContainer = document.querySelector('.ad-form__photo');
 
 //Добавление превью аватарки:
 fieldAvatarUpload.addEventListener('change', () => {
-  const file = fieldAvatarUpload.files[0];
+  const [file] = fieldAvatarUpload.files;
   const fileName = file.name.toLowerCase();
 
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
@@ -24,17 +24,17 @@ const createImage = (fileAddress) => {
   image.style = 'width: 100%; height: 100%; object-fit: cover';
   image.src = fileAddress;
 
-  fotoPreviewContainer.append(image);
+  photoPreviewContainer.append(image);
 };
 
-fieldFotoUpload.addEventListener('change', () => {
-  const file = fieldFotoUpload.files[0];
+fieldPhotoUpload.addEventListener('change', () => {
+  const [file] = fieldPhotoUpload.files;
   const fileName = file.name.toLowerCase();
 
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
-    fotoPreviewContainer.innerHTML = '';
+    photoPreviewContainer.innerHTML = '';
     createImage(URL.createObjectURL(file));
   }
 });
@@ -42,7 +42,7 @@ fieldFotoUpload.addEventListener('change', () => {
 //Функция для очистки полей предпросмотра изображений:
 const resetImages = () => {
   avatarPreview.src = 'img/muffin-grey.svg';
-  fotoPreviewContainer.innerHTML = '';
+  photoPreviewContainer.innerHTML = '';
 };
 
 export {resetImages};
